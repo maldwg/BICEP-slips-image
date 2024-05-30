4a39273641f40191e01ce44e35bb9624195ca342
maldwg <maxludwig3@t-online.de>
2024-05-30 09:20:42 +0200

    Upgrade slips image to use python3.12

--- bicep-slips/src/main.py
+++ bicep-slips/src/main.py
@@ -1,3 +1,21 @@
-import time
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .routers import crud, ids, ensemble
+app = FastAPI()
 
-print("hello world")
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def get_test():
+    return {"message": "test"}
