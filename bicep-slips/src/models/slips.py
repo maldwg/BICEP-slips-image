8aaf76bf4823ee36bfc157cee7dc69d47f248121
maldwg <maxludwig3@t-online.de>
2024-06-06 13:04:03 +0200

    add dummy methods to slips

--- bicep-slips/src/models/slips.py
+++ bicep-slips/src/models/slips.py
@@ -1,9 +1,11 @@
 from  src.utils.models.ids_base import IDSBase
 from fastapi import UploadFile
+from src.utils.fastapi.routes import tell_core_analysis_has_finished
 import shutil
 
 class Slips(IDSBase):
-    configuration_location = "/tmp/slips.yaml"
+    configuration_location: str = "/tmp/slips.yaml"
+    container_id: int = None
 
     def configure(self, temporary_file):
         shutil.move(temporary_file, self.configuration_location)
@@ -11,3 +13,14 @@ class Slips(IDSBase):
     
     def configure_ruleset(self, temporary_file):
         return "No ruleset to patch"
+    
+    async def startNetworkAnalysis(self):
+        return "Started Network analysis"
+    
+    async def startStaticAnalysis(self, file_path, container_id):
+        self.container_id = container_id
+        return "Started Static Analysis"
+    
+    async def stopAnalysis(self):
+        await tell_core_analysis_has_finished(container_id=self.container_id)
+        return "Stopped analysis"
