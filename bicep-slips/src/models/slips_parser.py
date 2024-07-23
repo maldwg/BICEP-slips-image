f59c4f5a22753896fb67924c23252a1b42f4f450
maldwg <maxludwig3@t-online.de>
2024-07-23 14:30:32 +0200

    add todo for slips parser

--- bicep-slips/src/models/slips_parser.py
+++ bicep-slips/src/models/slips_parser.py
@@ -3,7 +3,7 @@ from src.utils.models.ids_base import IDSParser, Alert
 class SlipsParser(IDSParser):
     
     alertFileLocation = ""
-
+    # TODO 4: write slips parser
     async def parse_alerts_from_network_traffic():
         pass
     async def parse_alerts_from_network_traffic():
