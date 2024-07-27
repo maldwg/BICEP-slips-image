f623e725a7b8b1f2c2972e195e33a432ef06e449
maldwg <maxludwig3@t-online.de>
2024-07-27 11:53:50 +0200

    Finish first draft slips parser

---
+++ bicep-slips/src/models/parser_test.py
@@ -0,0 +1,76 @@
+import json
+
+
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+class IDSParser(ABC):
+    timestamp_format = '%Y-%m-%dT%H:%M:%S.%f%z'
+
+    @property
+    @abstractmethod
+    def alertFileLocation(self):
+        pass
+    @abstractmethod
+    def parse_alerts_from_file(self):
+        pass
+    @abstractmethod
+    def parse_alerts_from_network_traffic(self):
+        pass
+
+class Alert():
+    """
+    Class which contains the most important fields of an alert (one line of anomaly).
+    It presents a standardized interface for the different IDS to map their distinct alerts to.
+    """
+    time: datetime
+    source: str
+    destination: str
+    severity: int
+    type: str
+    message: str
+    def __str__(self):
+        return f"{self.time}, From: {self.source}, To: {self.destination}, Type: {self.type}, Content: {self.message}, Severity: {self.severity}"
+
+
+
+import json 
+from datetime import datetime
+
+class SlipsParser(IDSParser):
+    
+    alertFileLocation = ""
+    def parse_alerts_from_file(self, file_location=alertFileLocation):
+        
+        parsed_lines = []
+
+        with open(file_location, "r") as file:
+            for line in file:
+                line_as_json = json.loads(line)
+                parsed_lines.append(self.parse_line(line_as_json))
+        return parsed_lines
+    
+    # TODO 4. write slips network parser
+    def parse_alerts_from_network_traffic():
+        pass
+
+    def parse_line(self, line):
+        parsed_line = Alert()
+        # todo: check if Is detect time now correctt in slips? --> it is not --> find out if necessary to id or id in other means
+        parsed_line.time = datetime.strptime(line.get("DetectTime"), self.timestamp_format) 
+        # since it is an array, acces the first element, then get the ip, the result is also in an array
+        parsed_line.source = line.get("Source")[0].get("IP4")[0]
+        try:
+            parsed_line.destination = line.get("Target")[0].get("IP4")[0]
+        except TypeError as e:
+            # many time there is no target, hence leave the information empty then
+            parsed_line.destination = ""
+        parsed_line.message = line.get("Attach")[0].get("Content")
+        parsed_line.type = line.get("Category")
+        parsed_line.severity = line.get("accumulated_threat_level")
+
+        return parsed_line
+if __name__ == "__main__":
+    parser = SlipsParser()
+    res = parser.parse_alerts_from_file("./slips_alerts.json")
+    print(str(res[0]))
--- bicep-slips/src/models/slips_parser.py
+++ bicep-slips/src/models/slips_parser.py
@@ -1,10 +1,52 @@
 from src.utils.models.ids_base import IDSParser, Alert
+import json 
+import os 
+from datetime import datetime
 
 class SlipsParser(IDSParser):
     
     alertFileLocation = ""
-    # TODO 4: write slips parser
-    async def parse_alerts_from_network_traffic():
-        pass
-    async def parse_alerts_from_network_traffic():
-        pass
+    async def parse_alerts_from_network_traffic(self, file_location=alertFileLocation):
+        
+        parsed_lines = []
+
+        with open(file_location, "r") as file:
+            for line in file:
+                line_as_json = json.loads(line)
+                parsed_lines.append(self.parse_line(line_as_json))
+
+        # remove file to prevent double sending results after next execution
+        os.remove(file_location)
+    
+        return parsed_lines
+    
+    # TODO 11: Either refactor so that only one parse mtehod exists (botha re equivalent) or identify things that the modes seperate from each other
+    async def parse_alerts_from_network_traffic(self, file_location=alertFileLocation):
+        parsed_lines = []
+
+        with open(file_location, "r") as file:
+            for line in file:
+                line_as_json = json.loads(line)
+                parsed_lines.append(self.parse_line(line_as_json))
+
+        # remove file to prevent double sending results after next execution
+        os.remove(file_location)
+    
+        return parsed_lines
+    async def parse_line(self, line):
+        parsed_line = Alert()
+        # todo: check if Is detect time now correctt in slips? --> it is not --> check if necessaray and use other tehniques
+        parsed_line.time = datetime.strptime(line.get("DetectTime"), self.timestamp_format) 
+        # since it is an array, acces the first element, then get the ip, the result is also in an array
+        parsed_line.source = line.get("Source")[0].get("IP4")[0]
+        try:
+            parsed_line.destination = line.get("Target")[0].get("IP4")[0]
+        except TypeError as e:
+            # many time there is no target, hence leave the information empty then
+            parsed_line.destination = ""
+        parsed_line.message = line.get("Attach")[0].get("Content")
+        parsed_line.type = line.get("Category")[0]
+        # TODO 6: find out scale and adapt to it  --> from 0 to 1 or 0 to 10 ??
+        parsed_line.severity = line.get("accumulated_threat_level")
+
+        return parsed_line
