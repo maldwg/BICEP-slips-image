78e9572a2fcf64d8441ce1afc371148c56b25a77
maldwg <maxludwig3@t-online.de>
2025-12-15 12:31:03 +0100

    update utils and add Exception handling for initial configuration

--- bicep-slips/src/models/slips.py
+++ bicep-slips/src/models/slips.py
@@ -1,11 +1,12 @@
 import asyncio
-from  src.utils.models.ids_base import IDSBase
+from src.utils.models.ids_base import IDSBase
 import shutil
 import os
 from src.utils.general_utilities import execute_command_async
 from .slips_parser import SlipsParser
 from concurrent.futures import ProcessPoolExecutor
 import subprocess
+
 
 class Slips(IDSBase):
     configuration_location: str = "/tmp/slips.yaml"
@@ -16,29 +17,45 @@ class Slips(IDSBase):
     working_dir = "/StratosphereLinuxIPS"
     parser = SlipsParser()
 
-
     async def configure(self, temporary_file):
         shutil.move(temporary_file, self.configuration_location)
         try:
             os.mkdir(self.log_location)
             return "succesfully configured"
-        except Exception as e:
-            print(e)
-            return e
-    
+        except Exception:
+            raise HTTPException(
+                status_code=500,
+                detail="Exception occured occured while configuring suricata. Please check the confgiuration file again and make sure it is valid!",
+            )
+
     # method needs to be implemented,even if slips does not feature rulesets
     async def configure_ruleset(self, temporary_file):
         pass
-    
+
     async def execute_network_analysis_command(self):
         os.chdir(self.working_dir)
-        command = ["./slips.py", "-c", self.configuration_location, "-i", self.tap_interface_name, "-o", self.log_location]
+        command = [
+            "./slips.py",
+            "-c",
+            self.configuration_location,
+            "-i",
+            self.tap_interface_name,
+            "-o",
+            self.log_location,
+        ]
         pid = await execute_command_async(command)
         return pid
 
     async def execute_static_analysis_command(self, file_path):
         os.chdir(self.working_dir)
-        command = ["./slips.py", "-c", self.configuration_location, "-f", file_path, "-o", self.log_location]
+        command = [
+            "./slips.py",
+            "-c",
+            self.configuration_location,
+            "-f",
+            file_path,
+            "-o",
+            self.log_location,
+        ]
         pid = await execute_command_async(command)
         return pid
-    
